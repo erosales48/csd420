f758314d3054c44ca480093aecb0d2946a3d2b67
erosales48 <157036707+erosales48@users.noreply.github.com>
2024-08-16 23:02:31 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.csd420 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.csd420 to javafx.fxml;
+    exports org.example.csd420;
+}
---
+++ src/main/java/org/example/csd420/module01/Module01Assignment.java
@@ -0,0 +1,16 @@
+package org.example.csd420.module01;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class WriteDataFile extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
