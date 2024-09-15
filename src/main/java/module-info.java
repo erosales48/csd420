18ad93e188eda577a8f532790366be8a31c7a537
erosales48 <157036707+erosales48@users.noreply.github.com>
2024-09-15 00:43:00 -0400

    Add new JavaFX example in Module07 with CSS styling
    Introduced a new Java class `Module07` showcasing JavaFX usage with circles and styled using the new `myStyle.css`. Updated `module-info.java` to export the new module.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,12 @@
 module org.example.csd420 {
     requires javafx.graphics;
     requires javafx.controls;
+    requires javafx.fxml;
     requires jdk.jdi;
 
     opens org.example.csd420 to javafx.fxml;
     exports org.example.csd420;
     exports org.example.csd420.Module01;
+    exports org.example.csd420.Module07 to javafx.graphics;
     opens org.example.csd420.Module01 to javafx.fxml;
 }
---
+++ src/main/java/org/example/csd420/Module07/Module07.java
@@ -0,0 +1,54 @@
+package org.example.csd420.Module07;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.layout.HBox;
+
+public class Module07 extends Application {
+
+    public Module07() {
+        super();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        HBox hBox = new HBox(5);
+        Scene scene = new Scene(hBox, 480, 300);
+
+        scene.getStylesheets().add("myStyle.css");
+        Pane pane1 = new Pane();
+        Circle circle1 = new Circle(60, 150, 50);
+        pane1.getChildren().addAll(circle1);
+        pane1.getStyleClass().add("border");
+
+        Pane pane2 = new Pane();
+        Circle circle2 = new Circle(60, 150, 50);
+        pane2.getChildren().addAll(circle2);
+
+        Pane pane3 = new Pane();
+        Circle circle3 = new Circle(60, 150, 50);
+        pane3.getChildren().addAll(circle3);
+
+        Pane pane4 = new Pane();
+        Circle circle4 = new Circle(60, 150, 50);
+        pane4.getChildren().addAll(circle4);
+
+        circle1.getStyleClass().add("plainCircle");
+        circle2.getStyleClass().add("circleBorder");
+        circle3.setId("redCircle");
+        circle4.setId("greenCircle");
+
+        hBox.getChildren().addAll(pane1, pane2, pane3, pane4);
+
+        primaryStage.setTitle("Module 07");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
