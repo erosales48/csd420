f142d45b8c88bacf132ef1d3865bf63a0ea759fd
erosales48 <erosales48@pm.me>
2024-09-19 12:12:28 -0400

    Add EdgarThreeThreads class for Module 8 Assignment
    This class implements three threads to output random letters, numbers, and special characters to a display. It fulfills the assignment requirements by generating a minimum of 10,000 characters for each type.

---
+++ src/main/java/org/example/csd420/Module08/EdgarThreeThreads.java
@@ -0,0 +1,16 @@
+package org.example.csd420.Module08;
+
+/* Edgar D Rosales
+   19 September 2024
+   CSD420-J318 Advanced Java Programming (2247-DD)
+
+   Module 8 Assignment:
+   1. Create a class tittled <your first name>ThreeThreads.
+      - In this class, you are to use three threads to output three types of characters to a text area for display.
+        - In the first thread, you are to output random letter characters such as a, b, c, d ...
+        - In the second thread, you are to output random number digits such as  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        - In the third thread, you are to output random characters such as !, @, #, $, %, &, *
+      - Display a minimum of 10,000 oe each three sets.
+ */
+public class EdgarThreeThreads {
+}
