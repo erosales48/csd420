92c6ce5a1d0e5b757848021a6f2261fc1220ffe6
erosales48 <157036707+erosales48@users.noreply.github.com>
2024-09-19 17:11:23 -0400

    Add configuration files and multi-threading code
    Introduced inspection and UI Designer configurations for the project. Implemented the main method in EdgarThreeThreads.java that launches three threads for symbols, characters, and digits. These threads run concurrently to generate random output, ensuring the program completes efficiently.

--- src/main/java/org/example/csd420/Module08/EdgarThreeThreads.java
+++ src/main/java/org/example/csd420/Module08/EdgarThreeThreads.java
@@ -13,4 +13,54 @@ package org.example.csd420.Module08;
       - Display a minimum of 10,000 oe each three sets.
  */
 public class EdgarThreeThreads {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10000; i++) {
+            if (i % 60 == 0) {
+                System.out.println();
+            } else {
+                if (i % 5 == 0) {
+                    System.out.print(" ");
+                }
+            }
+            Thread symbolThread = new Thread(() -> System.out.print(getRandomSymbol()));
+            Thread charThread = new Thread(() -> System.out.print(getRandomChar()));
+            Thread digitThread = new Thread(() -> System.out.print(getRandomDigit()));
+
+            symbolThread.start();
+            charThread.start();
+            digitThread.start();
+
+            try {
+                symbolThread.join();
+                charThread.join();
+                digitThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Function to select random alpha char
+    private static char getRandomChar() {
+        String charArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int index = (int) (Math.random() * charArray.length());
+        return charArray.charAt(index);
+    }
+
+    // Function to select random symbol char
+    private static char getRandomSymbol() {
+        String symbolArray = "!@#$%&*";
+        int index = (int) (Math.random() * symbolArray.length());
+        return symbolArray.charAt(index);
+    }
+
+    // Function to select random digit
+    private static char getRandomDigit() {
+        String digitArray = "0123456789";
+        int index = (int) (Math.random() * digitArray.length());
+        return digitArray.charAt(index);
+    }
 }
